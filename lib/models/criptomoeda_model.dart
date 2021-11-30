609aecde55544cc1bad643213447df921cef9309
albertrein <guilhermealberto8@gmail.com>
2021-11-29 22:18:24 -0300

    Iniciando projeto.

---
+++ lib/models/criptomoeda_model.dart
@@ -0,0 +1,21 @@
+class CriptoMoeda{
+  String high;
+  String vol;
+  String last;
+  String buy;
+  String sell;
+  String open;
+  int date;
+
+  CriptoMoeda({
+    required this.high,
+    required this.vol,
+    required this.last,
+    required this.buy,
+    required this.sell,
+    required this.open,
+    required this.date
+  });
+
+  
+}
